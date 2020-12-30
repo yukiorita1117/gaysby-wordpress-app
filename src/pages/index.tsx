9df6e3b2dd42e65749474dc5b6c13ab6179283fe
折田幸夫 <3204704@gmail.com>
2020-12-30 17:00:08 +0900

    create ploto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,59 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage: React.FC = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+export default ({ data }) => {
+  return (
+    <Layout>
+      <SEO title="home" />
+      <h4>Posts</h4>
+      {data.allWordpressPost.edges.map(({ node }) => (
+        <div>
+          <p>{node.title}</p>
+          <div dangerouslySetInnerHTML={{ __html: node.content }} />
+        </div>
+      ))}
+      {data.allWordpressPost.edges.map(({ node }) => {
+        console.log("nodeの中身は？？？？", node)
+        return (
+          <div>
+            <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+          </div>
+        )
+      })}
+    </Layout>
+  )
+}
 
-export default IndexPage
+export const pageQuery = graphql`
+  query {
+    allWordpressPost(sort: { fields: [date] }) {
+      edges {
+        node {
+          title
+          content
+          excerpt
+        }
+      }
+    }
+  }
+`
+
+// const IndexPage: React.FC = () => (
+//   <Layout>
+//     <SEO title="Home" />
+//     <h1>Hi people</h1>
+//     <p>Welcome to your new Gatsby site.</p>
+//     <p>Now go build something great.</p>
+//     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+//       <Image />
+//     </div>
+
+//     <Link to="/page-2/">Go to page 2</Link> <br />
+//     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+//   </Layout>
+// )
+// export default IndexPage
